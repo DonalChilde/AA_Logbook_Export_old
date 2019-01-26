af1387cd5b7ec99d8b62eb49184a46112609fadf
Chad <39224079+DonalChilde@users.noreply.github.com>
2019-01-26 05:29:48 -0700

    see previous

--- Python/src/airportsDB/airportsDB.py
+++ Python/src/airportsDB/airportsDB.py
@@ -3,6 +3,8 @@ will provide interface for airport lookup.
 load db to in memory sqlite? or just lookup from json.
 simplest solution first, then play and do speed testing.
 
+Data Source: https://github.com/mwgg/Airports
+
 TODO: https://python-packaging.readthedocs.io/en/latest/non-code-files.html
 """
 import json
