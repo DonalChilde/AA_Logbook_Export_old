9d6b6561f2e22aaf435f42540e813fdbc7f2b6db
Chad <39224079+DonalChilde@users.noreply.github.com>
2019-01-25 20:08:46 -0700

    fixed imports

--- Python/src/aaLogbook/airportsDB.py
+++ Python/src/aaLogbook/airportsDB.py
@@ -2,6 +2,8 @@
 will provide interface for airport lookup.
 load db to in memory sqlite? or just lookup from json.
 simplest solution first, then play and do speed testing.
+
+TODO: https://python-packaging.readthedocs.io/en/latest/non-code-files.html
 """
 import json
 import logging
--- Python/tests/test_timeDelta.py
+++ Python/tests/test_timeDelta.py
@@ -1,4 +1,4 @@
-from aaLogbook.timeDelta import timedelta_To_isoformat
+from utilities.timedelta_util import timedelta_To_isoformat
 from datetime import timedelta
 
 #days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0
