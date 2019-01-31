6f0ff88e0bd4c2119def97eba5d98914791fa76a
Chad <39224079+DonalChilde@users.noreply.github.com>
2019-01-30 17:45:17 -0700

    timedeltasplit, hhmmss output format

--- Python/src/utilities/timedelta_util.py
+++ Python/src/utilities/timedelta_util.py
@@ -1,5 +1,6 @@
 
 from datetime import timedelta
+from dataclasses import dataclass
 
 def parse_isoformatDuration(durationString: str)-> timedelta:
     pass
@@ -17,11 +18,17 @@ def parse_HHdotMM_To_timedelta(durationString: str, separator:str = ".")-> timed
     hours, minutes = map(int, (hours, minutes))# type: ignore
     return timedelta(hours=hours, minutes=minutes)# type: ignore
 
+@dataclass
+class TimeDeltaSplit():
+    days:int = 0
+    hours:int = 0
+    minutes:int = 0
+    seconds:int = 0
+    microseconds:int = 0
 
-def timedelta_To_isoformat(timeDelta: timedelta, strict=True)->str:
-    """
-    if strict then limit output fields to PddDThhHmmMss.sS # Not implemeted
-    """
+
+
+def timedelta_split(timeDelta:timedelta)-> TimeDeltaSplit:
     int_seconds = 0
     if timeDelta.days:
         int_seconds = int_seconds + (abs(timeDelta.days)*86400)
@@ -31,20 +38,41 @@ def timedelta_To_isoformat(timeDelta: timedelta, strict=True)->str:
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
     microseconds = timeDelta.microseconds
+    return TimeDeltaSplit(days = days,hours = hours,minutes = minutes,seconds = seconds, microseconds = microseconds)
+
+def timeDelta_TO_HHMMSS(timeDelta:timedelta, separator:str = ':'):
+    timeSplit = timedelta_split(timeDelta)
+    totalHours = (timeSplit.days * 24)+timeSplit.hours
+    return(f"{totalHours}{separator}{timeSplit.minutes}{separator}{timeSplit.seconds}.{timeSplit.microseconds:06d}")
+
+def timedelta_To_isoformat(timeDelta: timedelta, strict=True)->str:
+    """
+    if strict then limit output fields to PddDThhHmmMss.sS # Not implemeted
+    """
+    # int_seconds = 0
+    # if timeDelta.days:
+    #     int_seconds = int_seconds + (abs(timeDelta.days)*86400)
+    # if timeDelta.seconds:
+    #     int_seconds = int_seconds + timeDelta.seconds
+    # minutes, seconds = divmod(int_seconds, 60)
+    # hours, minutes = divmod(minutes, 60)
+    # days, hours = divmod(hours, 24)
+    # microseconds = timeDelta.microseconds
+    timeSplit = timedelta_split(timeDelta)
     daystext = hourstext = minutestext = secondstext = microtext = ""
-    if days:
-        daystext = f"{days}D"
-    if hours:
-        hourstext = f"{hours}H"
-    if minutes:
-        minutestext = f"{minutes}M"
-    if microseconds:
-        if not seconds:
-            seconds = 0
-        microtext = f".{microseconds:06d}"
-    if seconds or microseconds:
-        secondstext = f"{seconds}{microtext}S"
-    if not (hours or minutes or seconds or microseconds):
-        secondstext = f"{seconds}S"
+    if timeSplit.days:
+        daystext = f"{timeSplit.days}D"
+    if timeSplit.hours:
+        hourstext = f"{timeSplit.hours}H"
+    if timeSplit.minutes:
+        minutestext = f"{timeSplit.minutes}M"
+    if timeSplit.microseconds:
+        if not timeSplit.seconds:
+            timeSplit.seconds = 0
+        microtext = f".{timeSplit.microseconds:06d}"
+    if timeSplit.seconds or timeSplit.microseconds:
+        secondstext = f"{timeSplit.seconds}{microtext}S"
+    if not (timeSplit.hours or timeSplit.minutes or timeSplit.seconds or timeSplit.microseconds):
+        secondstext = f"{timeSplit.seconds}S"
     isoString = f"P{daystext}T{hourstext}{minutestext}{secondstext}"
     return isoString
