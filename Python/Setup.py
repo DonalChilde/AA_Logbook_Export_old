94f0a483f29c1380e85cb2259326117469e297bf
Chad <39224079+DonalChilde@users.noreply.github.com>
2019-02-01 20:32:54 -0700

    release 0.1.0

--- Python/Setup.py
+++ Python/Setup.py
@@ -20,7 +20,7 @@ URL = 'https://github.com/DonalChilde/AA_Logbook_Export'
 EMAIL = 'pfmsoft@gmail.com'
 AUTHOR = 'Chad'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.0.1.dev'
+VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
